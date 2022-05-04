86cc9ef5a29e4fa7b037b58ba4b4a9818124070b
Dennis lam <denwt.lam@outlook.com>
2022-05-04 13:58:44 -0400

    base design of website

---
+++ pages/projects.js
@@ -0,0 +1,16 @@
+import {
+    Flex,
+    Heading,
+    Text,
+    Link
+} from '@chakra-ui/react'
+import Container from '../components/Container'
+
+export default function Projects(){
+
+    return (
+      <Container>
+        <Heading>To be implemented</Heading>
+      </Container>
+    );
+}
