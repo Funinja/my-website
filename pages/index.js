d508cc31c4c7debcfede09fdbe7dfef3efe4c823
Dennis Lam <denwt.lam@outlook.com>
2022-05-19 21:17:36 -0400

    added personal info

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,45 @@
 import Container from "../components/Container";
-import { Heading, Text, Button } from "@chakra-ui/react";
+import { Flex, Center, Heading, Text, Image, Spacer } from "@chakra-ui/react";
+
 
 export default function Home() {
   return (
     <>
       <Container>
-        <Heading as="h1" size="3xl">
-          Dennis Lam
-        </Heading>
-        <Text fontSize="2xl" my={4}>
-          Software  Student
+
+        <Flex
+        flexDirection='row'
+        width='740px'>
+
+          <Flex
+            flexDirection='column' 
+            >
+            <Spacer />
+            <Heading as="h1" size="3xl">
+              Dennis Lam
+            </Heading>
+            <Text fontSize="2xl" my={4}>
+              Software Student
+            </Text>
+            <Spacer />
+          </Flex>
+          
+          <Spacer />
+
+
+          <Image 
+          borderRadius='full'
+          boxSize='180px'
+          src='/my_pic.jpg'
+          alt='Dennis Lam' 
+          />
+
+
+        </Flex>
+
+        <Text fontSize="2xl" my={10}>
+          Welcome to my website! Currently, I am a Computer Engineer graduating in 2023 at the University of Toronto - St. George Campus.
+          I have an interest in low-level programming, art, and reading.
         </Text>
         {/* <Button colorScheme="cyan" size="lg">
           Hire
